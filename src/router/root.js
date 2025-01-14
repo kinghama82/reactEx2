43291701cc0e8e2f1f05e28fb0a046817b3ca8eb
kinghama82 <wngjsrb88@naver.com>
2025-01-14 12:51:33 +0900

    2025-01-14 리액트학원수업

--- src/router/root.js
+++ src/router/root.js
@@ -4,6 +4,7 @@ const {createBrowserRouter} = require("react-router-dom");
 const Loading = <div>Loading 중입니다</div>
 const Main = lazy(() => import("../pages/MainPage"));
 const About = lazy(() => import("../pages/AboutPage"));
+const TodoIndex = lazy(() => import("../pages/todo/IndexPage"));
 
 //기본라우팅 설정(부트 메인컨트롤러 역할?)
 const root = createBrowserRouter([
@@ -15,6 +16,11 @@ const root = createBrowserRouter([
     {   //path는 중괄호 한개당 한개만 들어감( url 주소니까 )
         path: 'about',
         element: <Suspense fallback={Loading}><About/></Suspense>
+    },
+
+    {   //path는 중괄호 한개당 한개만 들어감( url 주소니까 )
+        path: 'todo',
+        element: <Suspense fallback={Loading}><TodoIndex/></Suspense>
     }
 ])
 
